1c87229a773e7422ef18965a8141bd0a75e825b4
daersh <daersh@naver.com>
2024-04-03 11:04:34 +0900

    Initial commit

---
+++ bootProject/src/main/java/com/ohgiraffers/bootproject/config/WebConfig.java
@@ -0,0 +1,31 @@
+package com.ohgiraffers.bootproject.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/*
+    *  Rest-API 서버측에서 CORS 처리를 위한 설정 클래스이다.
+* */
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+//                특정 포트들만 추가할 시!!!@!@!@!@
+//               .allowedOrigins(
+//                          "http://localhost:5173",
+//                          "http://localhost:5173",
+//                          "http://localhost:5173"
+//               )
+                .allowedMethods("GET","POST","PUT","DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+}
